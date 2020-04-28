47ab08af4e3b1dde1c1daa46363542e4e352e24e
Vanessa <vanessa.watson1@pcc.edu>
2020-04-28 15:54:34 -0700

    Added author

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,6 +1,11 @@
 package edu.pcc.cis.helloworld;
 import java.util.Date;
 
+/**
+ * Say hello world project
+ * @author Vanessa Watson
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
